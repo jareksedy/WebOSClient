69a6ad649b9e536ba7fbebf8da8633014033a925
Yaroslav Sedyshev <yaroslavse@halykbank.kz>
2023-12-29 14:20:51 +0600

    Change platform versions.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "WebOSClient",
-    platforms: [.iOS("7.0"), .macOS("10.15"), .watchOS("5.0"), .tvOS("9.0")],
+    platforms: [.iOS("13.0"), .macOS("10.15"), .watchOS("5.0"), .tvOS("13.0")],
     products: [
         .library(
             name: "WebOSClient",
