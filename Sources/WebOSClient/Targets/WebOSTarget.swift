9d5684adadf9a7c1d0d305732935003f442a154f
Yaroslav Sedyshev <yaroslavse@halykbank.kz>
2024-01-04 14:10:52 +0600

    Document WebOSTarget.

--- Sources/WebOSClient/Targets/WebOSTarget.swift
+++ Sources/WebOSClient/Targets/WebOSTarget.swift
@@ -5,35 +5,118 @@
 
 import Foundation
 
+/// Enum defining various WebOS targets for communication with LG TVs.
 public enum WebOSTarget {
+    /// Registers the client with an optional client key.
+    /// - Parameter clientKey: The optional client key for registration. If nil, prompt to register appears on TV screen.
     case register(clientKey: String?)
+    
+    /// Increases the volume.
     case volumeUp
+    
+    /// Decreases the volume.
     case volumeDown
+    
+    /// Retrieves the current volume level with optional subscription.
+    /// - Parameter subscribe: If true, subscribes to volume changes; if false, unsubscribes from volume changes; If nil, retrieves current volume level.
     case getVolume(subscribe: Bool? = nil)
-    case setVolume(Int)
-    case setMute(Bool)
+    
+    /// Sets the volume to the specified level.
+    /// - Parameter level: The desired volume level to set 0 to 100.
+    case setVolume(_ level: Int)
+    
+    /// Mutes or unmutes the audio.
+    /// - Parameter mute: If true, mutes the audio; if false, unmutes the audio.
+    case setMute(_ mute: Bool)
+    
+    /// Initiates playback.
     case play
+    
+    /// Pauses the current media playback.
     case pause
+    
+    /// Stops the current media playback.
     case stop
+    
+    /// Rewinds the current media playback.
     case rewind
+    
+    /// Fast-forwards the current media playback.
     case fastForward
+    
+    /// Retrieves the current sound output with optional subscription.
+    /// - Parameter subscribe: If true, subscribes to sound output changes; if false, unsubscribes from sound output changes;
     case getSoundOutput(subscribe: Bool? = nil)
-    case changeSoundOutput(WebOSSoundOutputType)
+    
+    /// Changes the sound output to the specified type.
+    /// - Parameter type: The desired sound output type. See WebOSSoundOutputType.
+    case changeSoundOutput(_ type: WebOSSoundOutputType)
+    
+    /// Shows a message on the screen, an optional icon data, and icon extension.
+    /// - Parameters:
+    ///   - message: The message to be displayed on the TV.
+    ///   - iconData: Optional icon data to be displayed along with the message.
+    ///   - iconExtension: Optional extension specifying the format of the icon data.
     case notify(message: String, iconData: Data? = nil, iconExtension: String? = nil)
+    
+    /// Turns off the TV screen.
     case screenOff
+    
+    /// Turns on the TV screen.
     case screenOn
+    
+    /// Retrieves system information.
     case systemInfo
+    
+    /// Turns off the TV.
     case turnOff
+    
+    /// Retrieves a list of installed apps.
     case listApps
+    
+    /// Retrieves the foreground app with optional subscription.
+    /// - Parameter subscribe: If true, subscribes to foreground app changes; if false, unsubscribes from foreground app changes; if nil, no subscription.
     case getForegroundApp(subscribe: Bool? = nil)
+    
+    /// Launches an app with the specified ID, content ID, and parameters.
+    /// - Parameters:
+    ///   - appId: The ID of the app to be launched.
+    ///   - contentId: Optional content ID associated with the app.
+    ///   - params: Optional parameters to be passed to the app.
     case launchApp(appId: String, contentId: String? = nil, params: String? = nil)
+    
+    /// Closes the app with the specified ID and optional session ID.
+    /// - Parameters:
+    ///   - appId: The ID of the app to be closed.
+    ///   - sessionId: Optional session ID associated with the app.
     case closeApp(appId: String, sessionId: String? = nil)
+    
+    /// Inserts text in the text input field with an optional replacement index (keyboard must be open).
+    /// - Parameters:
+    ///   - text: The text to be inserted.
+    ///   - replace: Optional index specifying the position for text replacement.
     case insertText(text: String, replace: Int = 0)
+    
+    /// Sends an enter key press to the TV.
     case sendEnterKey
+    
+    /// Deletes a specified number of characters from the text input.
+    /// - Parameter count: The number of characters to be deleted.
     case deleteCharacters(count: Int = 1)
+    
+    /// Retrieves the input socket for pointer input. This is sent automatically once the client gets registered.
     case getPointerInputSocket
+    
+    /// Increases the TV channel.
     case channelUp
+    
+    /// Decreases the TV channel.
     case channelDown
+    
+    /// Retrieves a list of available sources.
     case listSources
-    case setSource(inputId: String)
+    
+    /// Sets the TV source to the specified input ID.
+    /// - Parameter inputId: The ID of the input source to be set.
+    case setSource(_ inputId: String)
 }
