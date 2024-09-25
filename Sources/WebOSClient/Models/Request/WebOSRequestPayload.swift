b719d5d3a44895623671d4f435e7ae7f38e12fb1
hmduc1603 <hmduc1603@gmail.com>
2024-09-25 12:10:20 +0100

    Add init

--- Sources/WebOSClient/Models/Request/WebOSRequestPayload.swift
+++ Sources/WebOSClient/Models/Request/WebOSRequestPayload.swift
@@ -29,6 +29,54 @@ public struct WebOSRequestPayload: Codable {
     var category: String?
     var settings: SystemSettings?
 
+    public init(
+        pin: String? = nil,
+        forcePairing: Bool? = nil,
+        manifest: WebOSRequestManifest? = nil,
+        pairingType: String? = nil,
+        clientKey: String? = nil,
+        message: String? = nil,
+        iconData: Data? = nil,
+        iconExtension: String? = nil,
+        volume: Int? = nil,
+        mute: Bool? = nil,
+        output: String? = nil,
+        standbyMode: String? = nil,
+        id: String? = nil,
+        contentId: String? = nil,
+        params: String? = nil,
+        sessionId: String? = nil,
+        text: String? = nil,
+        replace: Bool? = nil,
+        count: Int? = nil,
+        inputId: String? = nil,
+        category: String? = nil,
+        settings: SystemSettings? = nil
+    ) {
+        self.pin = pin
+        self.forcePairing = forcePairing
+        self.manifest = manifest
+        self.pairingType = pairingType
+        self.clientKey = clientKey
+        self.message = message
+        self.iconData = iconData
+        self.iconExtension = iconExtension
+        self.volume = volume
+        self.mute = mute
+        self.output = output
+        self.standbyMode = standbyMode
+        self.id = id
+        self.contentId = contentId
+        self.params = params
+        self.sessionId = sessionId
+        self.text = text
+        self.replace = replace
+        self.count = count
+        self.inputId = inputId
+        self.category = category
+        self.settings = settings
+    }
+
     public enum CodingKeys: String, CodingKey {
         case clientKey = "client-key"
         case pin
@@ -53,8 +101,13 @@ public struct WebOSRequestPayload: Codable {
         case category
         case settings
     }
+
 }
 
 public struct SystemSettings: Codable {
     var brightness: Int
+
+    public init(brightness: Int) {
+        self.brightness = brightness
+    }
 }
